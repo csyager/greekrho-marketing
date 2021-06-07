b9f5da24ab7ab8c8d8d1747870000978779d2d23
Connor Yager <csy7ay@virginia.edu>
2021-06-06 22:59:30 -0400

    mocked up home page with carousel of stock images and a few elements

--- src/App.js
+++ src/App.js
@@ -5,27 +5,19 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
+import Home from './components/Home.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
